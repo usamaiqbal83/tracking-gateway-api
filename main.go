c8fa57e3fa4c3b207c60112753028bb3d9482973
Usama <usama_iqbal83@yahoo.com>
2018-06-19 21:50:29 +0500

    gateway api created

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 )
 
 const PORT = 9000
+const apiURL = "https://staging.leadfuze.com"
+
+//https://lfclk.co/trk/o?t=ODY2NXwyNTYyMnwxMDY4Mjc3MQ==
 
 func main() {
-	http.HandleFunc("/", trackOpenGateway)
+	http.HandleFunc("/trk/o", trackOpenGateway)
 	err := http.ListenAndServe(":"+strconv.Itoa(PORT), nil)
 	if err != nil {
 		panic(err.Error())
@@ -16,6 +21,35 @@ func main() {
 }
 
 func trackOpenGateway(w http.ResponseWriter, r *http.Request) {
-	test := "Hello World"
-	w.Write([]byte(test))
+	switch r.Method {
+	case "GET":
+		token := r.URL.Query().Get("t")
+		if token != ""{
+			url := fmt.Sprintf(apiURL+"/trk/o?t=%s",token)
+			fmt.Println("URL : ", url)
+			res, err := http.Get(url)
+			if err != nil {
+				fmt.Println("Error : ", err.Error())
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			} else {
+				defer res.Body.Close()
+				contents, err := ioutil.ReadAll(res.Body)
+				if err != nil {
+					fmt.Println("Error : ", err.Error())
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+				w.WriteHeader(res.StatusCode)
+				w.Write(contents)
+				return
+			}
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internet Server Error"))
+	}
 }
